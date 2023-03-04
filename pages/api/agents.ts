9b8a90941dd339c62c7b93fff157ab3b5e7a84c3
David Laban <alsuren@gmail.com>
2023-03-04 16:37:56 +0000

    try getting /api/agents working too

--- pages/api/agents.ts
+++ pages/api/agents.ts
@@ -4,6 +4,7 @@ import Redis from "ioredis";
 export default async (request: NextApiRequest, response: NextApiResponse) => {
   let year = parseInt(request.query.year?.toString(), 10);
   let month = parseInt(request.query.month?.toString(), 10);
+  let day = parseInt(request.query.day?.toString(), 10);
   if (isNaN(year) || year <= 0) {
     const now = new Date();
     year = now.getUTCFullYear();
@@ -12,21 +13,26 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
     const now = new Date();
     month = now.getUTCMonth() + 1;
   }
-  const counts = await get_stats(year, month);
+  if (isNaN(day) || day <= 0) {
+    const now = new Date();
+    day = now.getUTCDate();
+  }
+  const counts = await get_stats(year, month, day);
   response.status(200).send(JSON.stringify(counts));
 };
 
 // FIXME: this is copy-pasta from stats.ts. Make a shared helper somewhere instead.
 const get_stats = async (
   year: number,
-  month: number
+  month: number,
+  day: number,
 ): Promise<Record<string, number>> => {
   const result: Record<string, number> = {};
   let client = new Redis(process.env.REDIS_URL);
   let cursor = "0";
   let values;
   while (true) {
-    [cursor, values] = await client.hscan(`agents/${year}/${month}`, cursor);
+    [cursor, values] = await client.hscan(`agents/${year}/${month}/${day}`, cursor);
     for (let i = 0; i < values.length; i += 2) {
       result[values[i]] = Number(values[i + 1]);
     }
